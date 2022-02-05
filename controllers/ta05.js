7fbb8a314438edb7de291e2c10a5fd1cc7db0af8
bubbzDotDev <christopher.blount@outlook.com>
2022-02-04 22:41:17 -0600

    Finished the rest of the requirements

--- controllers/ta05.js
+++ controllers/ta05.js
@@ -1,13 +1,33 @@
 
 exports.getIndex = (req, res, next) => {
+  if (req.session.counter === undefined) {
+    req.session.counter = 0;
+  }
+  if (req.session.style === undefined) {
+    req.session.style = 0;
+  }
   res.render('pages/ta05', {
     title: 'Team Activity 05',
     path: '/ta05',
-    style: req.session.style
+    style: req.session.style,
+    counter: req.session.counter
   });
 };
 
 exports.postChangeStyle = (req, res, next) => {
   req.session.style = req.body.cssColor;
   res.redirect('/ta05');
+};
+
+exports.postCounter = (req, res, next) => {
+  req.session.counter += +req.body.counter;
+  res.redirect('/ta05');
+};
+
+exports.postResetSession = (req, res, next) => {
+  if (req.body.reset === 'true') {
+    req.session.destroy(() => {
+      res.redirect('/ta05');
+    });
+  }
 };
--- routes/ta05.js
+++ routes/ta05.js
@@ -5,6 +5,10 @@ const ta05Controller = require('../controllers/ta05');
 
 router.post('/change-style', ta05Controller.postChangeStyle);
 
+router.post('/counter', ta05Controller.postCounter);
+
+router.post('/reset', ta05Controller.postResetSession);
+
 router.get('/', ta05Controller.getIndex);
 
 module.exports = router;
